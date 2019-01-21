9ffaf37718f4904e5618cfc5ed9f695378b41f0f
ynnadkrap <dannypark92@gmail.com>
2019-01-21 18:14:39 -0500

    Write test for TeamSerializer

---
+++ spec/serializers/team_serializer_spec.rb
@@ -0,0 +1,15 @@
+describe TeamSerializer do
+  let!(:team) { build(:team, public_id: 1) }
+
+  it 'serializes correctly' do
+    res = JSON.parse(TeamSerializer.render(team)).with_indifferent_access
+
+    expect(res[:id]).to eq team.public_id
+    expect(team[:conference]).to eq team.conference
+    expect(team[:division]).to eq team.division
+    expect(team[:city]).to eq team.city
+    expect(team[:abbreviation]).to eq team.abbreviation
+    expect(team[:name]).to eq team.name
+    expect(team[:full_name]).to eq team.full_name
+  end
+end
