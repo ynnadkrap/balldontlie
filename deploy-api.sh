06e66b48df5dae7ea6a932fb44d36323b5b3dfad
Danny Park <dannypark92@gmail.com>
2019-03-07 12:26:07 -0500

    Write deploy scripts

---
+++ deploy-api.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Release version required"
+    exit 1
+  fi
+
+docker build -t gcr.io/balldontlie/api:$1 .
+docker push gcr.io/balldontlie/api:$1
+sed -i '' -E "s/version:.*$/version: $1/g" deploy/api/Chart.yaml
+sed -i '' -E "s/gcr.io\/balldontlie\/api:.*$/gcr.io\/balldontlie\/api:$1/g" deploy/api/templates/deployment.yaml
+helm upgrade unsung-worm deploy/api
+commit -am "Release api $1"
---
+++ deploy-client.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Release version required"
+    exit 1
+  fi
+
+docker build -t gcr.io/balldontlie/client:$1 ./frontend
+docker push gcr.io/balldontlie/client:$1
+sed -i '' -E "s/version:.*$/version: $1/g" deploy/client/Chart.yaml
+sed -i '' -E "s/gcr.io\/balldontlie\/client:.*$/gcr.io\/balldontlie\/client:$1/g" deploy/client/templates/deployment.yaml
+helm upgrade geared-jellyfish deploy/client
+commit -am "Release client $1"
