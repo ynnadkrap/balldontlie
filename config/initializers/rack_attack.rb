050bdaed025f1f8193011da2d818cdd7f780aabe
Danny Park <dannypark92@gmail.com>
2019-05-17 11:25:15 -0400

    Add ip blacklist

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
-Rack::Attack.throttle('requests by ip', limit: 60, period: 60) do |request|
-  request.ip
+class Rack::Attack
+  throttle('requests by ip', limit: 60, period: 60) do |request|
+    request.ip
+  end
+
+  spammers = ENV['ip_blacklist'].split(/,\s*/)
+  spammer_regexp = Regexp.union(spammers)
+  blocklist('block spammers') do |request|
+    request.ip =~ spammer_regexp
+  end
 end
